809d10fe1d9bd6fc23f4649b436d51bece244464
Isac Moura <contatoisacmoura@outlook.com>
2018-07-20 17:40:41 -0300

    Implementação da API

---
+++ frontend/src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3333',
+});
+
+class App extends Component {
+  state = {
+    newPostContent: '',
+    posts: [],
+  };
+
+  handlePostSave = async (e) => {
+    e.preventDefault();
+
+    const { data: post } = await api.post('/posts', { content: this.state.newPostContent});
+
+    this.setState({ posts: [... this.state.posts, post], newPostContent: ''});
+  };
+
+  async componentDidMount() {
+    //Buscar todos os posts da base de dados
+    const { data : posts } = await api.get('/posts');
+    
+    this.setState({ posts });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handlePostSave}>
+          <textarea
+          onChange={e => this.setState({ newPostContent: e.target.value})}
+          value={this.state.newPostContent}/>
+          <button type="submit">Postar</button>
+        </form>
+
+        <ul>
+          {this.state.posts.map(post => (
+            <li key={post.id}>{post.content}</li>
+          ))}
+          
+        </ul>
+
+      </div>
+    );
+  }
+}
+
+export default App;
